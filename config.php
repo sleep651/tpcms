2439396fa062f6efe64888b8e744199cb8f840c2
sleep651 <59937368@qq.com>
2014-11-07 11:21:42 +0800

    新增config.php
    新增config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+return array (
+  'db_type' => 'mysql',
+  'db_host' => 'localhost',
+  'db_name' => 'cms',
+  'db_user' => 'root',
+  'db_pwd' => 'root',
+  'db_port' => 3306,
+  'db_prefix' => 'tp_',
+  'web_name' => 'CMS网',
+  'web_url' => 'http://cms.demo.com/',
+  'web_path' => '/',
+  'web_icp' => '粤ICP备000000号',
+  'CRON_MAX_TIME' => 60,
+  'web_copyright' => '',
+  'web_tongji' => '',
+  'web_admin_pagenum' => 20,
+  'web_home_pagenum' => 15,
+  'web_adsensepath' => 'Public/Banner',
+);
+?>
